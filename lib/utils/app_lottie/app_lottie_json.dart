7b94dcbe5f95331d699c1f19865720a835bb5e5b
eusouf12 <riyajuddeneusouf@gmail.com>
2026-02-13 16:22:43 +0600

    make the private project to public project for add it my portfolio

--- lib/utils/app_lottie/app_lottie_json.dart
+++
@@ -1,6 +0,0 @@
-class AppLottieJson {
-  static const String basePath = "assets/lottie";
-  ///=========================Nab bar Icon ======================
-  static const String recoring = "$basePath/recoring.json";
-
-}
